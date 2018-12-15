193af6940f1cb89d99dfa85b74187236479907d5
akoszsebe <zsebea@yahoo.com>
2018-12-15 14:33:07 +0200

    try appdynamics

--- backend/routes.js
+++ backend/routes.js
@@ -3,18 +3,6 @@
 let path = require('path');
 let request = require("request");
 
-require("appdynamics").profile({
-  controllerHostName: 'amelia201812141654263.saas.appdynamics.com',
-  controllerPort: 443, 
-  // If SSL, be sure to enable the next line
-  controllerSslEnabled: true,
-  accountName: 'amelia201812141654263',
-  accountAccessKey: '9dv8hvd09f56',
-  applicationName: 'TemperatureServer',
-  tierName: 'TierSeccond',
-  nodeName: 'process' // The controller will automatically append the node name with a unique number
- });
-
 let temperature = 23;
 
 module.exports = (app) => {
--- index.js
+++ index.js
@@ -5,6 +5,17 @@ let express = require('express'),
   bodyParser = require('body-parser'),
   http = require('http').Server(app)
 
+  require("appdynamics").profile({
+    controllerHostName: 'amelia201812141654263.saas.appdynamics.com',
+    controllerPort: 443, 
+    // If SSL, be sure to enable the next line
+    controllerSslEnabled: true,
+    accountName: 'amelia201812141654263',
+    accountAccessKey: '9dv8hvd09f56',
+    applicationName: 'TemperatureServer',
+    tierName: 'TierSeccond',
+    nodeName: 'process' // The controller will automatically append the node name with a unique number
+   });
 
 // set port
 app.set('port', process.env.PORT || 8080);
